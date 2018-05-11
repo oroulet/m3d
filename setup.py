f75efce9b8fdf746d949ac2ad85f862714e720ba
oroulet <olivier@loke>
2018-05-11 17:30:06 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="m3d",
+    version="0.1.1",
+    description="Non GPL math3d alternative",
+    author="Olivier Roulet-Dubonnet",
+    author_email="olivier.roulet@gmail.com",
+    url='https://gitlab.com/kurant/m3d',
+    packages=find_packages(),
+    provides=["m3d"],
+    license="MIT",
+    install_requires="numpy",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
