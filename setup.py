cf3f6f868a01a82e9b99e31adc31109578be768b
oroulet <olivier@loke>
2018-07-16 11:08:50 +0200

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="m3d",
-    version="0.3.5",
+    version="0.3.6",
     description="Python3 3D transformation library with object oriented API and MIT licensed",
     author="Olivier Roulet-Dubonnet",
     author_email="olivier.roulet@gmail.com",
