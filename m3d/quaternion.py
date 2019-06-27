ef8c8e08343aa1f1bd7bc7e713cb0c5010d8a535
Rasmus <rasmusnes.t@gmail.com>
2019-06-27 16:32:15 +0200

    Add quaternion class.

---
+++ m3d/quaternion.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class Quaternion(object):
+    """
+    Represent a quaternion
+    Takes a list/array of length 4 as argument.
+    """
+    def __init__(self, x=[0, 0, 0, 0]):
+            if isinstance(x, (list, tuple)):
+                if len(x) == 4:
+                    self._data = np.array(x)
+                else:
+                    raise ValueError(f"A list of length 4 is expected, got {x}")
+            elif isinstance(x, np.ndarray):
+                if x.shape == (4,):
+                    self._data = x
+                else:
+                    raise ValueError(f"A array of shape (4,) is expected got {x}")
+            else:
+                raise ValueError(f"A array of shape (4,) is expected got {x}")
+
+
+    def __str__(self):
+        return "Quaternion({})".format(self._data)
+
+    __repr__ = __str__
+
+    @property
+    def data(self):
+        """
+        Access the numpy array used by Quaternion
+        """
+        return self._data
+
+    def __rmul__(self, other):
+        if isinstance(other, (float, int)):
+            return Quaternion(self._data * other)
+        else:
+            raise ValueError()
+
+    def __mul__(self, other):
+        if isinstance(other, Quaternion):
+            s1 = self._data[0]
+            s2 = other._data[0]
+            q1 = self._data[1:]
+            q2 = other._data[1:]
+            scalar_part = np.asarray([s1 * s2 - np.dot(q1, q2)])
+            imag_part = np.asarray(s1 * q2 + s2 * q1 + np.cross(q1, q2))
+            return np.hstack((scalar_part, imag_part))
+        elif isinstance(other, (int, float)):
+            return Quaternion(other * self._data)
+
+        else:
+            raise ValueError("Cannot multiply quaternion with type {}".format(type(other)))
+
+    @property
+    def conjugate(self):
+        return Quaternion([self._data[0], - self._data[1], - self.data[2], - self.data[3]])
+
+    def copy(self):
+        return Quaternion(self._data.copy())
