caa3a81557e87162c8bb582a481fe7c701cfa700
Rasmus Tjørstad <rasmusnes.t@gmail.com>
2019-06-27 16:31:28 +0200

    Add dual quaternion class.

---
+++ m3d/dual_quaternion.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+
+from m3d.common import float_eps
+from m3d.quaternion import Quaternion
+
+
+class DualQuaternion(object):
+
+
+    def __init__(self, vector=None):
+        if vector is not None:
+            self._data = vector
+
+    def __str__(self):
+        return "DualQuaternion(\n{}, \n{})".format(self._data[:4], self._data[4:])
+
+    __repr__ = __str__
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def q_rot(self):
+        return Quaternion(self._data[:4])
+    
+    @property
+    def q_trans(self):
+        return Quaternion(self._data[4:])
+    
+    @staticmethod
+    def from_transformation(trans):
+        """
+        Write the transformation as a dual quaternion. A dual quaternion can be
+        written as q = r + epslion * d
+        where the r quaternion is known as the real or rotational part and the
+        d quaternion is known as the dual or displacement part. epsilon is the
+        dual unit.
+        We denote q_rot as the rotation part and q_trans as the displacement
+        part.
+        """
+
+        q_rot = Quaternion(trans.orient.to_quaternion())
+        q_trans = 0.5 * Quaternion([0, *trans.pos.data]) * q_rot 
+        return DualQuaternion([*q_rot.data, *q_trans.data])
+
+    def __mul__(self, other):
+        if isinstance(other, DualQuaternion):
+            q_rot = self.q_rot * other.q_rot
+            q_trans = self.q_rot * other.q_trans + self.q_trans * other.q_rot
+            return DualQuaternion([*q_rot.data, *q_trans.data])
+        elif isinstance(other, (float, int)):
+            return DualQuaternion(other * self._data)
+        else:
+            raise ValueError()
+
+    def __rmul__(self, other):
+        if isinstance(other, (float, int)):
+            return DualQuaternion(self._data * other)
+        else:
+            raise ValueError()
+
+
+    @property
+    def conjugate(self):
+        return DualQuaternion([*self.q_rot.conjugate.data, *self.q_trans.conjugate.data])
+
+    @property
+    def unity_condition(self):
+        """
+        When working with rigid rotational and translational transformations
+        the dual quaternion must fulfill the unit condition, i.e.
+        it must be of unit length.
+        """
+        norm = np.linalg.norm((self * self.conjugate).data)
+        if abs(1 - norm) < float_eps:
+            return True
+        else:
+            return False
+
+        
